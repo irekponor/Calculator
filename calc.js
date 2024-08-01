5c192807b962279e5f5b2d7a961b2130f681146b
emmanuel <emmanuelirekponor86@gmail.com>
2024-08-01 03:21:32 +0100

    commit

---
+++ calc.js
@@ -0,0 +1,19 @@
+// Performing calc task
+
+const display = document.getElementById("display");
+
+function appendtoDisplay(input) {
+  display.value += input;
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "error";
+  }
+}
+
+function clearDisplay() {
+  display.value = "";
+}
