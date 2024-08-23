6d3088a5a833e63b741114eabc45df2dcb371898
emmanuel <emmanuelirekponor86@gmail.com>
2024-08-23 20:24:36 +0100

    mee

--- calc.js
+++ calc.js
@@ -3,17 +3,4 @@
 const display = document.getElementById("display");
 
 function appendtoDisplay(input) {
-  display.value += input;
-}
 
-function calculate() {
-  try {
-    display.value = eval(display.value);
-  } catch (error) {
-    display.value = "error";
-  }
-}
-
-function clearDisplay() {
-  display.value = "";
-}
