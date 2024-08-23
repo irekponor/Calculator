3feb44132be9f33fb690bac4aed9ac9cfd596492
emmanuel <emmanuelirekponor86@gmail.com>
2024-08-23 20:21:38 +0100

    calculator

--- calc.js
+++ calc.js
@@ -3,4 +3,17 @@
 const display = document.getElementById("display");
 
 function appendtoDisplay(input) {
+  display.value += input;
+}
 
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "error";
+  }
+}
+
+function clearDisplay() {
+  display.value = "";
+}
